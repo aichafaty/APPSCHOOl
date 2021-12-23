49fcc50f98e31269d535c3cf54a5d5aea3c763f8
aichafaty <aichafatybittyba@gmail.com>
2021-12-23 13:53:11 +0100

    verification de la saisie de l'utilisateur avec le nomvre limite et le refut de l'excecution du boutton d'ajout sans la saisie

--- src/scool.js
+++ src/scool.js
@@ -76,41 +76,44 @@ function verifier() {
     console.log("Nom = " + nom.value);
     console.log("Prenom = " + prenom.value);
     console.log("Niveau = " +niveau.value);
-    console.log("Biographie = " + bio.value);     
+    console.log("Biographie = " + bio.value);  
+    
+    
+     // VERIFICATION DES MOTS SAISIS
+    bio.addEventListener("input", (event) => {
+        const longueurMax = 130
+        const contenuSaisi = bio.value
+        const longueurSaisi = contenuSaisi.length
+        const reste = longueurMax - longueurSaisi
+    
+        //actualiser le dom pour afficher le nombre
+        const paragraphCompteur = document.getElementById("limite-text")
+        const compteurText = document.getElementById("text-progress")
+        const restantText = document.getElementById("text-restant")
+        
+        compteurText.textContent = longueurSaisi
+        restantText.textContent = " Il vous reste " + reste
+    
+        //changer couleur
+    
+        if (reste < 0) {
+          paragraphCompteur.style.color = "#ce0033"
+          ajouter.disabled = true
+        } else if (reste <= 16) {
+          paragraphCompteur.style.color = "yellow"
+          ajouter.disabled = false
+        } else {
+          paragraphCompteur.style.color = "#00000"
+          ajouter.disabled = false
+        }
+  })
+
 }
 
 
 
 
 
-//     // // VERIFICATION DES MOTS SAISIS
-//     // inputSuggestion.addEventListener("input", (event) => {
-//     //     const longueurMax = 130
-//     //     const contenuSaisi = inputSuggestion.value
-//     //     const longueurSaisi = contenuSaisi.length
-//     //     const reste = longueurMax - longueurSaisi
-    
-//     //     //actualiser le dom pour afficher le nombre
-//     //     const paragraphCompteur = document.getElementById("limite-text")
-//     //     const compteurText = document.getElementById("text-progress")
-//     //     const restantText = document.getElementById("text-restant")
-//     //     const btnSuggestion = document.getElementById("btn-suggestion")
-//     //     compteurText.textContent = longueurSaisi
-//     //     restantText.textContent = " Il vous reste " + reste
-    
-//     //     //changer couleur
-    
-//     //     if (reste < 0) {
-//     //       paragraphCompteur.style.color = "#ce0033"
-//     //       btnSuggestion.disabled = true
-//     //     } else if (reste <= 16) {
-//     //       paragraphCompteur.style.color = "yellow"
-//     //       btnSuggestion.disabled = false
-//     //     } else {
-//     //       paragraphCompteur.style.color = "#00000"
-//     //       btnSuggestion.disabled = false
-//     //     }
-//     //   })
 
 
 
