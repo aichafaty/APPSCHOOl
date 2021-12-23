1ea247c2dee76aa49b6ac87f63f8c1cdf8ecf521
aichafaty <aichafatybittyba@gmail.com>
2021-12-23 13:19:03 +0100

    ajout des donner saisie dans les cartes

--- src/scool.js
+++ src/scool.js
@@ -1,17 +1,54 @@
-window.addEventListener("load",function() {
+
         
     
     const schoolForm = document.querySelector("form")
     const apprenantes=document.getElementById("apprenantes")
     const nom=document.getElementById("nom")
+    //CREATION DU TABLEAU DE D'APPRENAT
+    const APPRENANTS =[]
     
     const prenom=document.getElementById("prenom")
-    const select=document.getElementById("select")
+    const niveau=document.getElementById("niveau")
     const bio=document.getElementById("bio")
     const ajouter=document.getElementById("ajouter")
 
 
-    const creerUneCarteApp = () => {
+
+
+    
+
+//recuperation des donnees du formulaire
+schoolForm.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    let nomSaisi=nom.value
+    let prenomSaisi=prenom.value
+    let niveauSaisi=niveau.value
+    let bioSaisie=bio.value
+    verifier()
+
+
+
+
+
+    //creation objet etudiant
+let newApp={
+    nomA:nomSaisi,
+    prenomA:prenomSaisi,
+    niveauA:niveauSaisi,
+    bioA:bioSaisie,
+}
+
+    APPRENANTS.push(newApp)
+    console.log(apprenantes.length)
+    creerUneCarteApp(newApp)
+
+
+   
+    
+
+})
+
+    const creerUneCarteApp = (apprenante) => {
         apprenantes.insertAdjacentHTML(  "afterend",`
         <div class="card mb-3" style="max-width: 540px;">
                     <div class="row g-0">
@@ -20,10 +57,10 @@ window.addEventListener("load",function() {
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
-                        <h5 class="card-title">Nom:</h5>
-                        <h5 class="card-title">Prenom:</h5>
-                        <h5 class="card-title">niveau:</h5>
-                        <p class="card-text"></p>
+                        <h5 class="card-title">Nom:${apprenante.nomA}</h5>
+                        <h5 class="card-title">Prenom:${apprenante.prenomA}</h5>
+                        <h5 class="card-title">niveau:${apprenante.niveauA}</h5>
+                        <p class="card-text">${apprenante.bioA}</p>
                        
                         </div>
                     </div>
@@ -32,28 +69,17 @@ window.addEventListener("load",function() {
       
      `);
 
-   }
-   creerUneCarteApp()
+   }  
 
-//recuperation des donnees du formulaire
-schoolForm.addEventListener("submit",(e)=>{
-    e.preventDefault()
-
-    verifier()
-})
 
 function verifier() {
-    let nomSaisi=nom.value.trim()
-    let prenomSaisi=prenom.value.trim()
-    let niveauSaisi=select.value.trim()
-    let bioSaisie=bio.value.trim()
-
-    console.log("Nom = " + nomSaisi);
-    console.log("Prenom = " + prenomSaisi);
-    console.log("Niveau = " +niveauSaisi);
-    console.log("Biographie = " + bioSaisie);
-
+    console.log("Nom = " + nom.value);
+    console.log("Prenom = " + prenom.value);
+    console.log("Niveau = " +niveau.value);
+    console.log("Biographie = " + bio.value);     
 }
+
+
 
 
 
@@ -87,4 +113,4 @@ function verifier() {
 //     //   })
 
 
-})
+
